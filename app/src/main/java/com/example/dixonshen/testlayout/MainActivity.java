54cedc4fe215b3349f5c6a9943e3835a3a84bf69
z003r98d <jiabao.sun.ext@siemens.com>
2017-03-02 17:22:28 +0800

    页面跳转ing

--- app/src/main/java/com/example/dixonshen/testlayout/MainActivity.java
+++ app/src/main/java/com/example/dixonshen/testlayout/MainActivity.java
@@ -1,47 +1,136 @@
 package com.example.dixonshen.testlayout;
 
-import android.os.Build;
+import android.content.Intent;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String testJson = "";
+
+    private static int CURRENT_DEPTH = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout linearLayout1 = new LinearLayout(this);
-        LinearLayout.LayoutParams llLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-        TextView attr = new TextView(this);
-        attr.setText("RAM");
-        if (Build.VERSION.SDK_INT >= 23) {
-            attr.setTextAppearance(R.style.attr_text);
-        } else {
-            attr.setTextAppearance(this, R.style.attr_text);
-        }
-        linearLayout1.addView(attr);
-        TextView val = new TextView(this);
-        val.setText("2G");
-        if (Build.VERSION.SDK_INT >= 23) {
-            val.setTextAppearance(R.style.val_text);
-        } else {
-            val.setTextAppearance(this, R.style.val_text);
-        }
-        linearLayout1.addView(val);
-        LinearLayout layout1 = (LinearLayout) findViewById(R.id.linearLayout1);
-        layout1.addView(linearLayout1, llLayoutParams);
+
+        View imgView = findViewById(R.id.arrow);
+        imgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfo(v);
+            }
+        });
+
+
+        Map<String, Object> jsonMap = getMapFromJson(getJsonFromFile());
+
+
     }
+
+    public void init() {
+
+        final ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView1);
+
+        final Button btn1 = new Button(this);
+        btn1.setText("Get Json");
+        scrollView.addView(btn1);
+        final Map<String, Object> map = new HashMap<>();
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scrollView.removeView(btn1);
+                changePage(v, 1);
+            }
+        });
+
+    }
+
+    public void changePage(View v, int flag){
+
+        if (flag == 1){
+            this.CURRENT_DEPTH += 1;
+
+            showInfo(v);
+        } else {
+
+        }
+    }
+
+    public void showInfo(View v){
+
+        LinearLayout layout1 = (LinearLayout) findViewById(R.id.linearLayout1);
+
+        LinearLayout linearLayout = (LinearLayout)getLayoutInflater().inflate(R.layout.text_view1, null);
+        TextView attr = (TextView) linearLayout.findViewById(R.id.attr);
+        attr.setText("RAM");
+        TextView val = (TextView) linearLayout.findViewById(R.id.val);
+        val.setText("2G");
+
+        layout1.addView(linearLayout);
+
+        LinearLayout linearLayout2 = (LinearLayout)getLayoutInflater().inflate(R.layout.text_view2, null);
+        TextView attr2 = (TextView) linearLayout2.findViewById(R.id.attrs);
+        attr2.setText("RAM");
+        ImageView arrow = (ImageView) linearLayout2.findViewById(R.id.arrow);
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showInfo(v);
+            }
+        });
+
+        layout1.addView(linearLayout2);
+
+    }
+
 
     public Map<String, Object> getMapFromJson(String json) {
         Map<String, Object> result = new Gson().fromJson(json, new TypeToken<Map<String, Object>>(){}.getType());
         return result;
     }
+
+    public String getJsonFromFile() {
+        String resJson = "";
+        try {
+            AssetManager am = this.getAssets();
+            InputStream file = am.open("json1.txt");
+            InputStreamReader reader = new InputStreamReader(file);
+            BufferedReader br = new BufferedReader(reader);
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                resJson += line;
+            }
+            System.out.println(resJson);
+            reader.close();
+            file.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (resJson.equals("")) return null;
+            return resJson;
+        }
+    }
+
 }
